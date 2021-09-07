2160fb4337546db143cedd4c7cad8d4fa3315bb0
kimberly lin <kimberly.lin10@gmail.com>
2021-09-07 15:01:02 -0700

    serial class completed

--- serial.py
+++ serial.py
@@ -17,3 +17,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+
+    def __init__(self, start=0): #set default
+        """DOCSTRINGS"""
+        self.original_start = self.start = start
+        #self.start could be named current_num or something more specific
+    
+    # __repr__
+
+    def generate(self):
+        """DOCSTRINGS"""
+        self.start += 1
+        return self.start - 1
+    
+    def reset(self):
+        """DOCSTRINGS"""
+        self.start = self.original_start
